1befdbfe1e02c37ff48fdae3091fb4c843b6f305
phantom123 <akshoybehera12@gmail.com>
2023-02-21 17:27:11 +0530

    initial commit

---
+++ python2.py
@@ -0,0 +1,13 @@
+list1 = []
+for i in range (10):
+    list1.append(i)
+print(list1)
+n = len(list1)
+target = int(input('enter digit btwn 1-10 to find in the list...'))
+for i in range (n):
+    if target == list1[i]:
+        print("index value of target", i)    
+    else:
+        pass
+
+
